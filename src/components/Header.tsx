b8a9512d89afbe5b7ba64926a245389a7d964f89
frank harerimana <harfrank2@gmail.com>
2021-10-23 00:07:02 +0400

    resolve conflicts with sandbox io

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { AiFillCopyrightCircle } from "react-icons/ai";
 
 export default () => {
-    return (
-        <div className="bg-red-400 w-full">
-            <AiFillCopyrightCircle />
-        </div>
-    );
+  return (
+    <div className="bg-red-400 w-full">
+      <AiFillCopyrightCircle />
+    </div>
+  );
 };
