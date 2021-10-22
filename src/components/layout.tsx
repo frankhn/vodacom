52d9b2e8c999caa976a83469516270a9ff27e338
frankhn <harfrank2@gmail.com>
2021-10-22 22:12:39 +0200

    adds missing files

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "./Header";
 
 interface ILayout {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default ({ children }: ILayout) => {
-  return (
-    <div className="w-screen h-screen bg-red-500 px-5">
-      <Header />
-      {children}
-    </div>
-  );
+    return (
+        <div className="w-screen h-screen bg-red-500 px-5">
+            <Header />
+            {children}
+        </div>
+    );
 };
