e128af936fb655bfbe980d4f8bb215f239cdfd83
frankhn <harfrank2@gmail.com>
2021-10-22 22:03:41 +0200

    app header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AiFillCopyrightCircle } from "react-icons/ai";
+
+export default () => {
+    return (
+        <div className="bg-red-400 w-full">
+            <AiFillCopyrightCircle />
+        </div>
+    );
+};
