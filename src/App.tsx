44ee8cead9249f44971cd7bdeb06ecb2f47804e3
frankhn <harfrank2@gmail.com>
2021-10-22 22:03:33 +0200

    initial app  setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./styles.css";
+import React from "react";
+import Layout from "./components/layout";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Layout>
+      <div> some content</div>
+    </Layout>
   );
 }
