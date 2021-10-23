6e6cd64eef917666a6d12b7b78292a2c5f587908
frankhn <harfrank2@gmail.com>
2021-10-23 20:03:04 +0200

    layout header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,9 @@ import { AiFillCopyrightCircle } from "react-icons/ai";
 
 export default () => {
     return (
-        <div className="bg-red-400 w-full">
-            <AiFillCopyrightCircle />
+        <div className="flex flex-row bg-red-700 w-full p-4">
+            <div><AiFillCopyrightCircle size={50} /></div>
+            <div className="flex flex-col px-3 justify-center text-xl capitalize text-white">Portail d'inscription</div>
         </div>
     );
 };
