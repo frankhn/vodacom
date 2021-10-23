7512614f03add9265b2744003202a9fd151a068a
frankhn <harfrank2@gmail.com>
2021-10-23 20:02:24 +0200

    configures routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Layout from "./components/layout";
+import Auth from './components/auth/auth'
+import { AuthContextProvider } from './context/auth'
+import Home from "./components/Home";
+import CandidateVideos from "./components/candidateVideos";
+import ViewVideo from "./components/ViewVideo";
 
 export default function App() {
   return (
-    <Layout>
-      <div> some content</div>
-    </Layout>
+    <AuthContextProvider>
+      <Layout>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Auth} />
+            <Route path="/home" component={Home} />
+            <Route path="/candidate" component={CandidateVideos} />
+            <Route path="/video/:id" component={ViewVideo} />
+            <Route path="*" component={Auth} />
+          </Switch>
+        </BrowserRouter>
+      </Layout>
+    </AuthContextProvider>
   );
 }
